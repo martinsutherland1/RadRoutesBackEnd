f83489042279f004754b328994f8bca0faf60b6e
Martin Sutherland <martinsutherland1@gmail.com>
2021-06-10 10:10:50 +0100

    first commit

---
+++ src/test/java/com/RadRoutes/RadRoutesService/RadRoutesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RadRoutes.RadRoutesService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadRoutesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
