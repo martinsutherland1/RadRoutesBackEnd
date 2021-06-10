154d15eca036320be0568d437064510615cb4605
Martin Sutherland <martinsutherland1@gmail.com>
2021-06-10 15:02:50 +0100

    park controller done

--- src/main/java/com/RadRoutes/RadRoutesService/controller/ParkController.java
+++ src/main/java/com/RadRoutes/RadRoutesService/controller/ParkController.java
@@ -1,4 +1,52 @@
 package com.RadRoutes.RadRoutesService.controller;
 
+import com.RadRoutes.RadRoutesService.models.Park;
+import com.RadRoutes.RadRoutesService.repository.ParkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class ParkController {
+
+    @Autowired
+    ParkRepository parkRepository;
+
+    // INDEX
+    @GetMapping(value = "/parks")
+    public ResponseEntity<List<Park>> getAllParks(){
+        return new ResponseEntity<>(parkRepository.findAll(), HttpStatus.OK);
+    }
+
+    // GET
+    @GetMapping(value = "/parks/{id}")
+    public ResponseEntity getPark(@PathVariable Long id){
+        return new ResponseEntity<>(parkRepository.findById(id), HttpStatus.OK);
+    }
+
+    // POST
+    @PostMapping(value = "/parks")
+    public ResponseEntity<Park> postPark(@RequestBody Park park){
+        parkRepository.save(park);
+        return new ResponseEntity<>(park, HttpStatus.OK);
+    }
+
+    // UPDATE
+    @PatchMapping(value = "/park/{id}")
+    public ResponseEntity<Park> updatePark(@PathVariable Park park){
+        parkRepository.save(park);
+        return new ResponseEntity<>(park, HttpStatus.OK);
+    }
+
+    //DELETE
+    @DeleteMapping(value = "/PARKS/{id}")
+    public ResponseEntity<Park> deletePirate(@PathVariable Long id) {
+        Park found = parkRepository.getById(id);
+        parkRepository.delete(found);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
 }
