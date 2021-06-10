7dfdf72fa5817aee09a3331c19b814e60398d2a9
BrockBaron <Nslekkas@gmail.com>
2021-06-10 14:08:14 +0100

    repositories build

--- src/main/java/com/RadRoutes/RadRoutesService/repository/ParkRepository.java
+++ src/main/java/com/RadRoutes/RadRoutesService/repository/ParkRepository.java
@@ -1,4 +1,10 @@
 package com.RadRoutes.RadRoutesService.repository;
 
-public interface ParkRepository {
+
+import com.RadRoutes.RadRoutesService.models.Park;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkRepository extends JpaRepository<Park, Long> {
 }
--- src/main/java/com/RadRoutes/RadRoutesService/repository/UserRepository.java
+++ src/main/java/com/RadRoutes/RadRoutesService/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.RadRoutes.RadRoutesService.repository;
 
-public interface UserRepository {
+import com.RadRoutes.RadRoutesService.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
