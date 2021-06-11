c23f381f47024d2f0be8b4bd02d4215689d30976
Martin Sutherland <martinsutherland1@gmail.com>
2021-06-11 20:48:40 +0100

    2 tests passed

--- src/test/java/com/RadRoutes/RadRoutesService/RadRoutesServiceApplicationTests.java
+++ src/test/java/com/RadRoutes/RadRoutesService/RadRoutesServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.RadRoutes.RadRoutesService;
 
+import com.RadRoutes.RadRoutesService.models.Coordinate;
 import com.RadRoutes.RadRoutesService.models.Park;
 import com.RadRoutes.RadRoutesService.models.Route;
 import com.RadRoutes.RadRoutesService.models.User;
@@ -50,12 +51,69 @@ class RadRoutesServiceApplicationTests {
 		Route test2 = new Route("Easy on the legs",17.28,7.006,1070.05, "easy", 4, lochLomond);
 		routeRepository.save(test1);
 		routeRepository.save(test2);
-		List<Park> foundParks = parkRepository.findByParksId(1L);
-		assertEquals(1, foundParks.size());
 
+		Coordinate one1 = new Coordinate(56.340556, -4.366389, 228, test1);
+		coordinateRepository.save(one1);
+		Coordinate one2 = new Coordinate(56.320278, -4.405278, 314,test1);
+		coordinateRepository.save(one2);
+		Coordinate one3 = new Coordinate(56.287222, -4.391667, 250, test1);
+		coordinateRepository.save(one3);
+		Coordinate one4 = new Coordinate(56.2725, -4.351667, 358, test1);
+		coordinateRepository.save(one4);
+		Coordinate one5= new Coordinate(56.3375, -4.365833, 268, test1);
+		coordinateRepository.save(one5);
+		Coordinate one6 = new Coordinate(56.3375, -4.365833, 268, test1);
+		coordinateRepository.save(one6);
+		Coordinate two1 = new Coordinate(56.340556, -4.366389, 228, test2);
+		coordinateRepository.save(two1);
+		Coordinate two2 = new Coordinate(56.320278, -4.405278, 314,test2);
+		coordinateRepository.save(two2);
+		Coordinate two3 = new Coordinate(56.287222, -4.391667, 250, test2);
+		coordinateRepository.save(two3);
+		Coordinate two4 = new Coordinate(56.2725, -4.351667, 358, test2);
+		coordinateRepository.save(two4);
+		Coordinate two5= new Coordinate(56.3375, -4.365833, 268, test2);
+		coordinateRepository.save(two5);
+		Coordinate two6 = new Coordinate(56.3375, -4.365833, 268, test2);
+		coordinateRepository.save(two6);
+
+		test1.addCoordinate(one1);
+		test1.addCoordinate(one2);
+		test1.addCoordinate(one3);
+		test1.addCoordinate(one4);
+		test1.addCoordinate(one5);
+		test1.addCoordinate(one6);
+		routeRepository.save(test1);
+
+		test2.addCoordinate(two1);
+		test2.addCoordinate(two2);
+		test2.addCoordinate(two3);
+		test2.addCoordinate(two4);
+		test2.addCoordinate(two5);
+		test2.addCoordinate(two6);
+		routeRepository.save(test2);
 
 
 	}
+
+	@Test
+	public void parkHasRoute(){
+		List<Park> foundParks = parkRepository.findParkById(1L);
+		assertEquals(1, foundParks.size());
+		assertEquals("Loch Lomond", foundParks.get(0).getParkName());
+		assertEquals(2, foundParks.get(0).getAllRoutes().size());
+		assertEquals("Sore legs", foundParks.get(0).getAllRoutes().get(0).getRouteName());
+	}
+
+	@Test
+	public void userHasRoutes(){
+		List<User> foundUser = userRepository.findUserById(1L);
+		assertEquals(1, foundUser.size());
+		assertEquals("Martin", foundUser.get(0).getFirstName());
+		assertEquals(2, foundUser.get(0).getAllRoutes().size());
+		assertEquals("Easy on the legs", foundUser.get(0).getAllRoutes().get(1).getRouteName());
+	}
+
 
 
 
